e5ff20f92e6072a5401ea19cc28303343c973ed5
AliZakariaAlFateh <58048016+AliZakariaAlFateh@users.noreply.github.com>
2023-11-02 22:40:36 +0200

    Day 5 For This Project

--- Angular_Project/src/app/app.module.ts
+++ Angular_Project/src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { CarouselComponent } from './Components/carousel/carousel.component';
-import { ProductsComponent } from './Components/products/products.component';
-import { ProductCardComponent } from './Components/product-card/product-card.component';
-import { FooterComponent } from './Components/footer/footer.component';
+import { NavbarComponent } from './Components/Shared/navbar/navbar.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
 import { SideBarComponent } from './Components/side-bar/side-bar.component';
-
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './Components/Core/home/home.component';
+import { StudentComponent } from './Components/Core/student/student.component';
+import { ContactComponent } from './Components/Core/contact/contact.component';
+import { NotFoundComponent } from './Components/Core/not-found/not-found.component';
+import { StudentDetailsComponent } from './Components/Core/student-details/student-details.component';
+import { StudentFormComponent } from './Components/Core/student-form/student-form.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent,
-    ProductsComponent,
-    ProductCardComponent,
     FooterComponent,
-    SideBarComponent
+    SideBarComponent,
+    HomeComponent,
+    ContactComponent,
+    NotFoundComponent,
+    StudentDetailsComponent,
+    StudentFormComponent,
+    StudentComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
